68ba3743efd08cda06e504c3c7fd452f27d8deda
Matthias LECONTE <pixmat@gmail.com>
2017-03-02 18:51:40 +0100

    Cities and Countries are included into the store. + AddCompany form now fetch cities, countries and tags.

--- src/client/actions/people.js
+++ src/client/actions/people.js
@@ -5,3 +5,16 @@ export const loadPeople = () => ({
   type: LOAD_PEOPLE,
   replyTo: PEOPLE_LOADED,
 });
+
+export const makeAll = R.map(make);
+export const make = (person) => {
+  const updatedPerson = {
+    ...person,
+    typeName: 'person',
+    createdAt: moment(person.createdAt),
+  };
+  if (person.updatedAt) updatedPerson.updatedAt = moment(person.updatedAt);
+  return updatedPerson;
+}
+
+export default { loadPeople };
