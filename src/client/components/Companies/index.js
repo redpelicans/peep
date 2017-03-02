81249971015523bd9af474d38678bf656bc02ce0
sleli42 <lubaujar@e2r3p12.42.fr>
2017-03-02 18:09:38 +0100

    Fixes #16 && add tests, add text ellipsis

--- src/client/components/Companies/index.js
+++ src/client/components/Companies/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ListCompany from './List';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ListCompany } from './List';
+import { loadCompanies } from '../../actions/companies';
 
-const Companies = () =>
-  <ListCompany/>
-  ;
+/*
+  - change a -> link
+  - ended tests
+  - issue text elipsis
+*/
 
-export default Companies;
+export class Companies extends Component {
+  componentWillMount() {
+    const { loadCompanies } = this.props;
+    loadCompanies();
+  }
+  render() {
+    const { companies } = this.props;
+
+    return (
+      <ListCompany companies={companies} loadCompanies={loadCompanies} />
+    );
+  }
+}
+
+Companies.propTypes = {
+  companies: PropTypes.array.isRequired,
+  loadCompanies: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state;
+const mapDispatchToProps = dispatch => ({
+  loadCompanies: bindActionCreators(loadCompanies, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Companies);
