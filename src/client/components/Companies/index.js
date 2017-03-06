4b76968ae774e318b3441e012600f0c421136127
Matthias LECONTE <pixmat@gmail.com>
2017-03-06 12:28:03 +0100

    AddButton.

--- src/client/components/Companies/index.js
+++ src/client/components/Companies/index.js
@@ -1,14 +1,28 @@
-import R from 'ramda';
 import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Input, Icon } from 'antd';
-import styled from 'styled-components';
 import { List } from './List';
 import { togglePreferredFilter, togglePreferred, loadCompanies, filterCompanyList } from '../../actions/companies';
 import { PreferredFilter, TitleIcon, Header, HeaderLeft, HeaderRight, Title, Search } from '../widgets';
 import { getVisibleCompanies } from '../../selectors/companies';
+import { AddButton } from '../Button/';
+
+const HeaderCompanies = ({ onFilter, filter }) => (
+  <Header>
+    <HeaderLeft>
+      <TitleIcon name="home" />
+      <Title title="Companies" />
+    </HeaderLeft>
+    <HeaderRight>
+      <Search filter={filter} onChange={onFilter} />
+    </HeaderRight>
+  </Header>
+);
+
+HeaderCompanies.propTypes = {
+  onFilter: React.PropTypes.func.isRequired,
+  filter: React.PropTypes.string.isRequired,
+};
 
 export class Companies extends Component {
 
@@ -29,25 +43,21 @@ export class Companies extends Component {
 
   render() {
     const { companies, filter = '', preferredFilter, filterCompanyList, togglePreferred } = this.props;
-      return (
-        <div>
-          <Header>
-            <HeaderLeft>
-              <TitleIcon name="home" />
-              <Title title='Companies' />
-            </HeaderLeft>
-            <HeaderRight>
-              <Search filter={filter} onChange={this.onFilterChange} />
-              <PreferredFilter active={preferredFilter} onChange={this.handlePreferredFilter} />
-            </HeaderRight>
-          </Header>
-          <div>
-            <Link to="/companies/add">
-              Add A company
-            </Link>
-          </div>
-          <List companies={companies} filterCompanyList={filterCompanyList} togglePreferred={togglePreferred} />
-        </div>
+    return (
+      <div>
+        <Header>
+          <HeaderLeft>
+            <TitleIcon name="home" />
+            <Title title="Companies" />
+          </HeaderLeft>
+          <HeaderRight>
+            <Search filter={filter} onChange={this.onFilterChange} />
+            <PreferredFilter active={preferredFilter} onChange={this.handlePreferredFilter} />
+          </HeaderRight>
+        </Header>
+        <AddButton to="/companies/add" />
+        <List companies={companies} filterCompanyList={filterCompanyList} togglePreferred={togglePreferred} />
+      </div>
     );
   }
 }
@@ -57,10 +67,12 @@ Companies.propTypes = {
   filter: PropTypes.string,
   loadCompanies: PropTypes.func.isRequired,
   filterCompanyList: PropTypes.func.isRequired,
+  preferredFilter: PropTypes.func,
+  togglePreferred: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({ 
-  companies: getVisibleCompanies(state), 
+const mapStateToProps = state => ({
+  companies: getVisibleCompanies(state),
   filter: state.companies.filter,
   preferredFilter: state.companies.preferredFilter,
 });
