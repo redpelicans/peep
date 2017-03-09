b17472c450ba0509500eb9407f59b0935a7f9e65
Sofiane KHATIR <skhatir@e2r3p14.42.fr>
2017-03-09 17:48:25 +0100

    Notes

--- src/client/components/Notes/index.js
+++ src/client/components/Notes/index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Card, Col, Row, Input, Icon } from 'antd';
+import { Card, Icon } from 'antd';
 import styled from 'styled-components';
 import moment from 'moment';
 import { loadNotes } from '../../actions/notes';
@@ -16,6 +16,8 @@ export const TitleIconElt = styled(Icon)`
 
 export const CardElt = styled(Card)`
   margin: 5px;
+  margin-left: auto;
+  margin-right: auto;
   display: inline-block;
   min-width: 250px;
   width: 100%;
@@ -25,85 +27,116 @@ export const CardElt = styled(Card)`
   font-size: 1em !important;
   font-weight: bold !important;
   &:hover {
-  	background: white !important;
-  	box-shadow: 5px 5px 20px 2px black !important;
+    background: white !important;
+    box-shadow: 2px 2px 10px black !important;
   }
 `;
 
 export const FooterElt = styled.div`
   font-weight: normal !important;
   display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
 `;
 
-export const FooterLine = styled.hr`
+export const FooterLineElt = styled.hr`
   margin-top: 10px;
   margin-bottom: 10px;
 `;
 
-// export const NoteWrapper = styled.div`
-//   width: auto;
-//   margin-left: auto;
-//   margin-right: auto;
-//   display: flex;
-//   flex-flow: column wrap;
-//   justify-content: center;
-//   max-height: 1500px;
-//   overflow: auto;
-// `;
-
-export const NoteWrapper = styled.div`
+export const NoteWrapperElt = styled.div`
   margin: 1.5em 0;
   padding: 0;
   column-gap: 1.5em;
-  column-count: 3;
+  columns: 350px;
+  justify-content: center;
 `;
 
+export const IconElt = styled.div`
+  display: flex;
+  margin-right: 4px;
+  flexFlow: column;
+  justifyContent: flex-end;
+  fontSize: 1.2em;
+  color: darkgrey;
+  &:hover {
+    cursor: pointer;
+    color: red;
+  }
+`;
+
+export const WrapFooterElementElt = styled.div`
+  display: flex;
+`;
+
+export const EntityTitle = styled.h3`
+  margin: 0px;
+  &:hover {
+    color: cadetblue;
+    cursor: pointer;
+  }
+`;
+
+const entityIcon = {
+  person: 'user',
+  mission: 'shopping-cart',
+  company: 'home',
+};
+
 export const Footer = ({ note, person, entity }) => {
-  if (!person) return null;
+  if (!person || !entity || !note) return null;
   const author = `${person.firstName} ${person.lastName}`;
   const dateFormat = moment(note.createdAt).format('dddd, MMMM Do YYYY');
   return (
-  	<FooterElt>
-    {
-      [
+    <FooterElt>
+      <WrapFooterElementElt >
         <Avatar
           key={dateFormat}
-          name={entity && entity.name ? entity.name : "M"}
-          color={entity && entity.avatar  ? entity.avatar.color : "darkgrey"}
+          name={entity && entity.name ? entity.name : 'M'}
+          color={entity && entity.avatar ? entity.avatar.color : 'darkgrey'}
           style={{ margin: '5px' }}
           showTooltip
-        />,
-        entity.name,
-        <br key={note._id} />,
-        dateFormat,
+        />
+        <IconElt>
+          <Icon onClick={() => alert('todo => viewEntity')} type={entityIcon[note.entityType]} />
+        </IconElt>
+        <div>
+          <EntityTitle onClick={() => alert('todo => viewEntity')} >{entity.name}</EntityTitle>
+          {dateFormat}
+        </div>
+      </WrapFooterElementElt>
+      <div>
         <Avatar
-          key={note._id, author}
+          key={note._id + author}
           name={author}
           color={person.avatar.color}
-          style={{ margin: '5px' }}
+          style={{ minWidth: '25px', minHeight: '25px', height: '25px', width: '25px' }}
           showTooltip
-        />,
-      ]
-    }
+        />
+      </div>
     </FooterElt>
-  )
-}
+  );
+};
 
 Footer.propTypes = {
   note: PropTypes.object.isRequired,
-}
+  person: PropTypes.object.isRequired,
+  entity: PropTypes.object.isRequired,
+};
 
 export const CardContent = ({ note, person, entity }) =>
   <div>
     {note.content}
-    <FooterLine />
+    <FooterLineElt />
     <Footer note={note} person={person} entity={entity} />
   </div>
 ;
 
 CardContent.propTypes = {
   note: PropTypes.object.isRequired,
-}
+  person: PropTypes.object.isRequired,
+  entity: PropTypes.object.isRequired,
+};
 
 export class Notes extends Component {
   componentWillMount() {
@@ -114,10 +147,9 @@ export class Notes extends Component {
   }
 
   findEntity(entityType, entityId) {
-  	const { companies, people } = this.props;
+    const { companies, people } = this.props;
     const entity = entityType === 'person' ? people.data[entityId] : companies.data[entityId];
-    console.log('type/entity-->', entityType ,entity);
-  	return entity ? entity : '';
+    return entity ? entity : '';
   }
 
   render() {
@@ -125,85 +157,44 @@ export class Notes extends Component {
     return(
       <div>
         <Header>
-            <HeaderLeft>
-              <TitleIcon name="pushpin-o" />
-              <Title title='Notes' />
-            </HeaderLeft>
-            <HeaderRight>
-            </HeaderRight>
-          </Header>
-        <NoteWrapper >
+          <HeaderLeft>
+            <TitleIcon name="pushpin-o" />
+            <Title title='Notes' />
+          </HeaderLeft>
+          <HeaderRight>
+            <Search />
+          </HeaderRight>
+        </Header>
+        <NoteWrapperElt >
         {
           notes.data.map(note => (
-            <div key={note._id} style={{ maxWidth: '350px' }} >
+            <div key={note.entityId, note._id} style={{ display: 'flex', justifyContent: 'center' }} >
+            <div key={note._id, note.name} style={{ width: '90%' }} >
               <CardElt key={note._id} bordered={false}>
                 <CardContent
+                  key={note._id + note.entityType}
                   note={note}
                   person={people.data[note.authorId]}
-                  entity={this.findEntity(note.entityType, note.entityId)} />
+                  entity={this.findEntity(note.entityType, note.entityId)}
+                />
               </CardElt>
+            </div>
             </div>))
         }
-        </NoteWrapper>
+        </NoteWrapperElt>
       </div>
-    )
+    );
   }
 }
 
-// export class Notes extends Component {
-//   componentWillMount() {
-//     const { loadNotes, loadPeople, loadCompanies } = this.props;
-//     loadNotes();
-//     loadPeople();
-//     loadCompanies();
-//   }
-
-//   findEntity(entityType, entityId) {
-//   	const { companies, people } = this.props;
-//     const entity = entityType === 'person' ? people.data[entityId] : companies.data[entityId];
-//     console.log('type/entity-->', entityType ,entity);
-//   	return entity ? entity : '';
-//   }
-
-//   render() {
-//     const { notes, people, companies } = this.props;
-//     return(
-//       <div>
-//         <Header>
-//             <HeaderLeft>
-//               <TitleIcon name="pushpin-o" />
-//               <Title title='Notes' />
-//             </HeaderLeft>
-//             <HeaderRight>
-//             </HeaderRight>
-//           </Header>
-//         <Row type="flex" justify="end" >
-//         {
-//           notes.data.map(note => (
-//             <Col key={note._id} sm={24} md={12} lg={8}>
-//               <CardElt key={note._id} bordered={false}>
-//                 <CardContent
-//                   note={note}
-//                   person={people.data[note.authorId]}
-//                   entity={this.findEntity(note.entityType, note.entityId)} />
-//               </CardElt>
-//             </Col>))
-//         }
-//         </Row>
-//       </div>
-//     )
-//   }
-// }
-
-// https://jsbin.com/tuxoqusaxu/2/edit?css,output
-// http://w3bits.com/css-masonry/
-
 Notes.propTypes = {
   notes: PropTypes.object.isRequired,
+  companies: PropTypes.object.isRequired,
+  people: PropTypes.object.isRequired,
   loadNotes: PropTypes.func.isRequired,
   loadPeople: PropTypes.func.isRequired,
   loadCompanies: PropTypes.func.isRequired,
-}
+};
 
 const mapStateToProps = state => state;
 const mapDispatchToProps = dispatch => ({
