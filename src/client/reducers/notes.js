1a207561875895236328f2eadc15413362b4cc58
Sofiane KHATIR <skhatir@e2r3p14.42.fr>
2017-03-11 17:02:04 +0100

    moment in reducer

--- src/client/reducers/notes.js
+++ src/client/reducers/notes.js
@@ -1,9 +1,19 @@
+import R from 'ramda';
+import moment from 'moment';
 import { NOTES_LOADED, FILTER_NOTES_LIST } from '../actions/notes';
+
+const make = (note) => {
+  const updatedNote = { ...note, typeName: 'note', createdAt: moment(note.createdAt).format('dddd, MMMM Do YYYY') };
+  if (note.updatedAt) updatedNote.updatedAt = moment(note.updatedAt).format('dddd, MMMM Do YYYY');
+  return updatedNote;
+};
+
+const makeAll = R.map(n => make(n));
 
 const notes = (state = { data: [] }, action) => {
   switch (action.type) {
     case NOTES_LOADED:
-      return { ...state, data: action.payload };
+      return { ...state, data: makeAll(action.payload) };
     case FILTER_NOTES_LIST:
       return { ...state, filter: action.filter };
     default:
@@ -12,4 +22,3 @@ const notes = (state = { data: [] }, action) => {
 };
 
 export default notes;
-
